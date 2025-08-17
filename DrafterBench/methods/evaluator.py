c213d54c2144ccd00c2bfc613f21479678550b61
Jibf <bji36@gatech.edu>
2025-08-17 19:03:00 -0400

    supported resume evaluation by '--results_path'
    Signed-off-by: Jibf <bji36@gatech.edu>

--- DrafterBench/methods/evaluator.py
+++ DrafterBench/methods/evaluator.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.abspath(".."))
 
 from DrafterBench.methods import metric
 from DrafterBench.utils.types import Score_builder
-from DrafterBench.methods.collect_result import collect_result
+from DrafterBench.methods.collect_result import collect_result, process_code, execute_code
 
 
 def openfile(file):
@@ -22,6 +22,17 @@ def savedate(data, jsonpath):
 
 
 def evaluator(result_path, eval_result, response_result):
+    if "Testpath" not in response_result:
+        pre_code = response_result["Response_code"]
+        test_code = process_code(pre_code)
+        test_info = execute_code(test_code)
+        response_result["Testpath"] = test_info
+    if "Groundpath" not in response_result:
+        ground_code = response_result["Groundtruth"]
+        test_ground_code = process_code(ground_code)
+        ground_info = execute_code(test_ground_code)
+        response_result["Groundpath"] = ground_info
+
     ground_details = metric.ground_check(response_result["Groundpath"])
     prompt_score = (
         Score_builder()
@@ -45,5 +56,5 @@ def evaluator(result_path, eval_result, response_result):
     del response_result["Testpath"]
     eval_result.append(response_result)
     eval_data = list(eval_result)
-    savedate(eval_data, result_path)
+    savedate(eval_data, result_path.replace(".json", "_score.json"))
     return eval_result
